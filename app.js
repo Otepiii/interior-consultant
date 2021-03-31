c88fcd1f73a32f0baab2e61f7f40d6fc2a045b3f
joseph Evangelista <oootep123@gmail.com>
2021-03-30 23:50:13 -0700

    added mobile responsiveness

---
+++ app.js
@@ -0,0 +1,18 @@
+const hamburgerBtn = document.querySelector(".hamburger");
+
+hamburgerBtn.addEventListener("click", toggleNav);
+
+function toggleNav() {
+    const nav = document.querySelector(".nav-items");
+
+    if (!nav.classList.contains("active")) {
+        nav.classList.add("active")
+        nav.classList.remove("close")
+        nav.style.display="flex"
+    }else {
+        nav.classList.remove("active")
+        nav.classList.add("close")
+        nav.style.display="none"
+
+    }
+} 
